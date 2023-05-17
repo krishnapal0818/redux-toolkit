f210c5ab00d476e6a6a7bca04a0f51735481551a
krishnapal.singh <krishnapal@gmail.com>
2023-05-17 15:46:50 +0530

    Complite redux toolkit.

---
+++ src/Redux/store.js
@@ -0,0 +1,23 @@
+import { createStore } from 'redux';
+
+// Initial state
+const initialState = {
+  count: 0,
+};
+
+// Reducer function
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return { ...state, count: state.count + 1 };
+    case 'DECREMENT':
+      return { ...state, count: state.count - 1 };
+    default:
+      return state;
+  }
+};
+
+// Create the store
+const store = createStore(reducer);
+
+export default store;
